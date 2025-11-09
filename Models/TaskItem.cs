1aedbb348c8c4f3e7ff813eef6f9e5df4989cc3c
vik37 <vik.zafirovski@gmail.com>
2025-11-09 22:43:56 +0100

    Tasker Page. Blazor Example.

---
+++ Models/TaskItem.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTasker.Models
+{
+	public class TaskItem
+	{
+		public Guid Id { get; set; }
+		[Required(ErrorMessage="Every task should have a name")]
+		public string Name { get; set; } = string.Empty;
+		public bool IsCompleted { get; set; } = false;
+	}
+}
